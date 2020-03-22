e909bbdb0d59b40118171c084852c202161e53e4
Inga Danilova <aminga@yandex.ru>
2020-03-22 19:00:10 +0300

    feat: add morse-decoder

--- src/index.js
+++ src/index.js
@@ -36,9 +36,55 @@ const MORSE_TABLE = {
     '----.':  '9',
     '-----':  '0',
 };
-
 function decode(expr) {
-    // write your solution here
+    let strMors = '';
+    let sol = ''; 
+    let a = expr.split('');
+    for (let i = 1; i < expr.length; i++) {
+        if (a[i] == '0' && a[i - 1] == '1') {
+            strMors += '.';
+            if (i % 10 == 9) {
+                strMors += ',';
+            }
+            i += 1;
+        }
+        else if (a[i] == '1' && a[i - 1] == '1') {
+            strMors += '-';
+            if (i % 10 == 9) {
+                strMors += ',';
+            }
+            i += 1;
+        }
+        else if (a[i] == '*' && a[i - 1] == '*') {
+            strMors += ' ,';
+            if (i % 10 == 9) {
+                strMors += ',';
+            }
+            i += 9;
+        }
+        else if (a[i] == '*' && a[i - 1] != '*') {
+            strMors += ' ,';
+            if (i % 10 == 9) {
+                strMors += ',';
+            }
+            i += 10;
+        }
+        else if (a[i] == '0' && a[i - 1] == '0') {
+            if (i % 10 == 9) {
+                strMors += ',';
+            }
+            i += 1;
+        }
+    }
+    let words = strMors.split(',');
+    for (let j = 0; j < words.length - 1; j++) {
+        if (words[j] == ' '){
+            sol += ' ';
+            j++;
+        }
+        sol +=  MORSE_TABLE[words[j]];
+    }
+    return sol;
 }
 
 module.exports = {
